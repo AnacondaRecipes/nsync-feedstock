5305f464634f942a7f883f6074c12d35119d7ed5
Kai Tietz <ktietz@anaconda.com>
2021-05-27 07:55:30 +0200

    fixed linking on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@ mkdir -p out
 cd out
 if [ $(uname) == Linux ]; then
 	CMAKE_EXE_LINKER_FLAGS_INIT="-lrt"
+else
+        export LDFLAGS="${LDFLAGS} -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
 fi
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
