5f000fe4f2c7f03eb2349146aa29a0b73fe227aa
Valentyna Khomits <vkhomits@anaconda.com>
2026-09-09 15:15:58 +0300

    Rebuild for windows-arm64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ cd out
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
       ..
 nmake
 nmake install
